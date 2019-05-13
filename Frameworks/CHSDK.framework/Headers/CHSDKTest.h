4822794c9076b8ebcf8cbffcf23b88083bf7c75d
Cheer_Harry <cheer_harry@163.com>
2019-05-13 10:38:45 +0800

    init framework

---
+++ Frameworks/CHSDK.framework/Headers/CHSDKTest.h
@@ -0,0 +1,19 @@
+//
+//  CHSDKTest.h
+//  CHSDK
+//
+//  Created by Cheer_Harry on 2019/5/13.
+//  Copyright © 2019年 Cheer_Harry. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CHSDKTest : NSObject
+
++ (void)test;
+
+@end
+
+NS_ASSUME_NONNULL_END
